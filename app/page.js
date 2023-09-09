c9f7180d690f7b972ef3c81f628158c4c3e7a9ca
lshoyfer <lshoyfer@gmail.com>
2023-09-08 21:43:24 -0400

    Improve design, improve appearance across different devices

--- app/page.js
+++ app/page.js
@@ -10,54 +10,59 @@ export default function Home() {
     return (
         <>
             <section className={styles.intro}>
-                <div className={`${styles.text} ${styles["intro-motto"]}`}>
-                    <span>Tutoring,</span>
-                    <br />
-                    <span>Reimagined</span>
-                </div>
-                <div className={`${styles.text} ${styles["intro-blurb"]}`}>
-                    Our diverse and experienced<br />
-                    platform of teachers will provide<br />
-                    the educational resources and <br />
-                    guidance of your dreams.
-                </div>
-                <div className={styles["intro-nav"]}>
-                    <Link href="/book">Book</Link>
-                    <div>
-                        Free Lessons
-                        <Image
-                            src="./link.svg"
-                            width={24}
-                            height={24}
-                            alt="link icon"
-                        />
+                <div className={styles["intro-container"]}>
+                    <div className={`${styles.text} ${styles["intro-motto"]}`}>
+                        <span>Tutoring,</span>
+                        <br />
+                        <span>Reimagined</span>
+                    </div>
+                    <div className={`${styles.text} ${styles["intro-blurb"]}`}>
+                        Our diverse and experienced<br />
+                        platform of teachers will provide<br />
+                        the educational resources and <br />
+                        guidance of your dreams.
+                    </div>
+                    <div className={styles["intro-nav"]}>
+                        <Link href="/book">Book</Link>
+                        <div>
+                            Free Lessons
+                            <Image
+                                src="./link.svg"
+                                width={24}
+                                height={24}
+                                alt="link icon"
+                            />
 
+                        </div>
                     </div>
                 </div>
             </section>
-
+            {/* 
             <Image
                 className={styles["girl-img"]}
                 src={girlImg}
                 alt="Student Girl in Blue Long Sleeve Shirt Holding White and Blue Book"
                 sizes="(max-width: 900px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 priority
-            />
+            /> */}
 
             <section className={`${styles.text} ${styles["teacher-info"]}`}>
-                <div className={styles["teacher-info-header-animation-container"]}>
-                    <div className={styles["teacher-info-header"]}>
-                        <div>Real teachers</div> 
-                        <div>with</div>
-                        <div>Real experience.</div>
+                <div className={styles["teacher-info-container"]}>
+                    <div className={styles["teacher-info-header-container"]}>
+                        <div className={styles["teacher-info-header"]}>
+                            <div>Real teachers</div>
+                            <div>with</div>
+                            <div>Real experience.</div>
+                        </div>
+                        <PathAnimation type="mobile" svgClassName={styles["mobile-svg"]} pathsClassName={styles["path-animation"]} />
                     </div>
-                    <PathAnimation pathsClassName={styles["path-animation"]}/>
+                    <div className={styles["teacher-info-caption"]}>
+                        Learn from our team of NYC Public<br />
+                        High School teachers, or opt for a<br />
+                        first-class field coaching expert.
+                    </div>
                 </div>
-                <div className={styles["teacher-info-caption"]}>
-                    Learn from our team of NYC Public<br />
-                    High School teachers, or opt for a<br />
-                    first-class field coaching expert.
-                </div>
+                <PathAnimation type="desktop" svgClassName={styles["desktop-svg"]} pathsClassName={styles["path-animation"]} />
                 <TeacherButton />
             </section>
         </>
--- components/PathAnimation.js
+++ components/PathAnimation.js
@@ -1,7 +1,11 @@
 "use client";
 import { useEffect, useState, useRef } from "react";
 
-export default function PathAnimation({ pathsClassName }) {
+/*
+    I sepeated this into a different component so that I could control 
+    whether to unmount it and thus call useEffect's cleanup
+*/
+function PathBusinessLogic({ pathsClassName, svgClassName, type }) {
     const svgElement = useRef();
     const path1 = useRef();
     const path2 = useRef();
@@ -38,8 +42,8 @@ export default function PathAnimation({ pathsClassName }) {
     }, []);
 
     return (
-        <div>
-            <svg ref={svgElement} width="108" height="128" viewBox="0 0 54 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <>
+            <svg ref={svgElement} className={svgClassName} width="108" height="128" viewBox="0 0 54 64" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                     className={pathsClassName}
                     ref={path1}
@@ -56,6 +60,25 @@ export default function PathAnimation({ pathsClassName }) {
                     d="M18.0264 61.2838C18.0264 45.7838 22.5264 43.7838 34.5264 43.7838C46.5264 43.7838 51 24.7838 51 2.78384" stroke="#5486B8" strokeWidth="5" strokeLinecap="round"
                 />
             </svg>
-        </div>
+        </>
     );
+}
+
+export default function PathAnimation(props) {
+    const [showDesktop, setShowDesktop] = useState(() => window.innerWidth > 614);
+
+    useEffect(() => {
+        const handleScreenSize = () => {
+            const width = window.innerWidth;
+            setShowDesktop(width > 614);
+        }
+        handleScreenSize();
+        
+        window.addEventListener("resize", handleScreenSize);
+        return () => window.removeEventListener("resize", handleScreenSize);
+    }, []);
+
+
+    const shouldRender = ((showDesktop && props.type === "desktop") || (!showDesktop && props.type === "mobile"));
+    return shouldRender ? <PathBusinessLogic {...props} /> : null;
 }
