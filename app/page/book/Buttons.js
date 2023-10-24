b1dd11b574d63d806497766a5dac1ee852768f70
lshoyfer <lshoyfer@gmail.com>
2023-10-24 18:08:03 -0400

    Fix using next/link on mailto link; Use console.error instead in Buttons.

--- app/page/book/Buttons.js
+++ app/page/book/Buttons.js
@@ -1,5 +1,4 @@
 "use client";
-import Link from "next/link";
 import styles from "./page.module.css";
 import { useState, useEffect } from "react";
 
@@ -11,7 +10,7 @@ export default function Buttons() {
             await navigator.clipboard.writeText("aim2highconsulting@gmail.com");
             setIsCopied(true);
         } catch (e) {
-            console.log("Unable to copy text to clipboard", e);
+            console.error("Unable to copy text to clipboard", e);
         }
     }
 
@@ -26,7 +25,7 @@ export default function Buttons() {
             <div className={styles.buttons}>
                 {/* <Link href="https://www.google.com/forms/about">Form Link</Link> */}
                 <button onClick={copyEmailToClipboard}>Copy Email</button>
-                <Link href="mailto:aim2highconsulting@gmail.com">Compose Email</Link>
+                <a href="mailto:aim2highconsulting@gmail.com">Compose Email</a>
             </div>
             {isCopied && <div className={styles["copied-popup"]}>Copied</div>}
         </>
